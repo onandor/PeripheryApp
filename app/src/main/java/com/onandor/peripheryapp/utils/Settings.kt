05adacc00ca32cab1a90e8040b3e60ad3c935e53
Oláh Nándor <olahnandi00@gmail.com>
2024-03-12 17:38:36 +0100

    Add DataStore wrapper missing from previous commit

---
+++ app/src/main/java/com/onandor/peripheryapp/utils/Settings.kt
@@ -0,0 +1,40 @@
+package com.onandor.peripheryapp.utils
+
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.intPreferencesKey
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.map
+import javax.inject.Inject
+
+object BtSettingKeys {
+    val KEYBOARD_LOCALE = intPreferencesKey("keyboard_locale")
+    val MOUSE_POLLING_RATE = intPreferencesKey("mouse_polling_rate")
+}
+
+class Settings @Inject constructor(
+    private val dataStore: DataStore<Preferences>
+) {
+
+    fun <T> observe(key: Preferences.Key<T>, emptyValue: T): Flow<T> {
+        return dataStore.data
+            .catch { emit(emptyPreferences()) }
+            .map { prefs -> prefs[key] ?: emptyValue }
+    }
+
+    suspend fun <T> get(key: Preferences.Key<T>, emptyValue: T): T {
+        return observe(key, emptyValue).first()
+    }
+
+    suspend fun <T> save(key: Preferences.Key<T>, value: T) {
+        dataStore.edit { prefs -> prefs[key] = value }
+    }
+
+    suspend fun <T> remove(key: Preferences.Key<T>) {
+        dataStore.edit { prefs -> prefs.remove(key) }
+    }
+}
