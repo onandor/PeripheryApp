ecda7b211a5b9e0dd7c692df6b6587e35e53226a
Oláh Nándor <olahnandi00@gmail.com>
2024-03-15 20:17:50 +0100

    Round corners of control surfaces

--- app/src/main/java/com/onandor/peripheryapp/kbm/ui/components/ExtendedButtonGrid.kt
+++ app/src/main/java/com/onandor/peripheryapp/kbm/ui/components/ExtendedButtonGrid.kt
@@ -4,7 +4,9 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -27,7 +29,6 @@ import androidx.compose.ui.unit.dp
 import com.onandor.peripheryapp.kbm.input.ButtonData
 import com.onandor.peripheryapp.kbm.input.ExtendedButtons
 import com.onandor.peripheryapp.kbm.input.KeyMapping
-import kotlin.math.exp
 
 @Composable
 fun Button(
@@ -58,104 +59,102 @@ fun Button(
 }
 
 @Composable
-private fun DefaultRow(
-    buttonModifier: Modifier,
+private fun ColumnScope.DefaultRows(
     toggledModifiers: Int,
     onButtonClick: (Int) -> Unit
 ) {
-    Surface {
-        Column {
-            Row {
-                Button(modifier = buttonModifier, data = ExtendedButtons.SpecialKeys.ESCAPE, onClick = onButtonClick)
-                Button(modifier = buttonModifier, data = ExtendedButtons.SpecialKeys.TAB, onClick = onButtonClick)
-                Button(modifier = buttonModifier, data = ExtendedButtons.SpecialKeys.HOME, onClick = onButtonClick)
-                Button(modifier = buttonModifier, data = ExtendedButtons.SpecialKeys.END, onClick = onButtonClick)
-                Button(modifier = buttonModifier, data = ExtendedButtons.SpecialKeys.DELETE, onClick = onButtonClick)
-                Button(modifier = buttonModifier, data = ExtendedButtons.Arrows.UP, onClick = onButtonClick)
-                Button(modifier = buttonModifier, data = ExtendedButtons.SpecialKeys.BACKSPACE, onClick = onButtonClick)
-            }
-            Row {
-                Button(
-                    modifier = buttonModifier,
-                    data = ExtendedButtons.Modifiers.L_CTRL,
-                    onClick = onButtonClick,
-                    toggled = toggledModifiers and KeyMapping.Modifiers.L_CTRL != 0
-                )
-                Button(
-                    modifier = buttonModifier,
-                    data = ExtendedButtons.Modifiers.SHIFT,
-                    onClick = onButtonClick,
-                    toggled = toggledModifiers and KeyMapping.Modifiers.L_SHIFT != 0
-                )
-                Button(
-                    modifier = buttonModifier,
-                    data = ExtendedButtons.Modifiers.L_ALT,
-                    onClick = onButtonClick,
-                    toggled = toggledModifiers and KeyMapping.Modifiers.L_ALT != 0
-                )
-                Button(
-                    modifier = buttonModifier,
-                    data = ExtendedButtons.Modifiers.R_ALT,
-                    onClick = onButtonClick,
-                    toggled = toggledModifiers and KeyMapping.Modifiers.R_ALT != 0
-                )
-                Button(modifier = buttonModifier, data = ExtendedButtons.Arrows.LEFT, onClick = onButtonClick)
-                Button(modifier = buttonModifier, data = ExtendedButtons.Arrows.DOWN, onClick = onButtonClick)
-                Button(modifier = buttonModifier, data = ExtendedButtons.Arrows.RIGHT, onClick = onButtonClick)
-            }
-        }
+    val modifier = Modifier
+        .weight(1f)
+        .aspectRatio(1f)
+
+    Row {
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.ESCAPE, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.TAB, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.HOME, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.END, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.DELETE, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.Arrows.UP, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.BACKSPACE, onClick = onButtonClick)
+    }
+    Row {
+        Button(
+            modifier = modifier,
+            data = ExtendedButtons.Modifiers.L_CTRL,
+            onClick = onButtonClick,
+            toggled = toggledModifiers and KeyMapping.Modifiers.L_CTRL != 0
+        )
+        Button(
+            modifier = modifier,
+            data = ExtendedButtons.Modifiers.SHIFT,
+            onClick = onButtonClick,
+            toggled = toggledModifiers and KeyMapping.Modifiers.L_SHIFT != 0
+        )
+        Button(
+            modifier = modifier,
+            data = ExtendedButtons.Modifiers.L_ALT,
+            onClick = onButtonClick,
+            toggled = toggledModifiers and KeyMapping.Modifiers.L_ALT != 0
+        )
+        Button(
+            modifier = modifier,
+            data = ExtendedButtons.Modifiers.R_ALT,
+            onClick = onButtonClick,
+            toggled = toggledModifiers and KeyMapping.Modifiers.R_ALT != 0
+        )
+        Button(modifier = modifier, data = ExtendedButtons.Arrows.LEFT, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.Arrows.DOWN, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.Arrows.RIGHT, onClick = onButtonClick)
     }
 }
 
 @Composable
-private fun ExpandedRow(
-    btnModifier: Modifier,
+private fun ColumnScope.ExpandedRows(
     toggledModifiers: Int,
     onButtonClick: (Int) -> Unit
 ) {
-    Surface {
-        Column {
-            Row {
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F1, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F2, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F3, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F4, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F5, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F6, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F7, onClick = onButtonClick)
-            }
-            Row {
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F8, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F9, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F10, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F11, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.FRow.F12, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.SpecialKeys.INSERT, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.SpecialKeys.PAGE_UP, onClick = onButtonClick)
-            }
-            Row {
-                Button(
-                    modifier = btnModifier,
-                    data = ExtendedButtons.Modifiers.L_META,
-                    onClick = onButtonClick,
-                    toggled = toggledModifiers and KeyMapping.Modifiers.L_META != 0
-                )
-                Button(modifier = btnModifier,
-                    data = ExtendedButtons.Modifiers.R_META,
-                    onClick = onButtonClick,
-                    toggled = toggledModifiers and KeyMapping.Modifiers.R_META != 0
-                )
-                Button(modifier = btnModifier,
-                    data = ExtendedButtons.Modifiers.R_CTRL,
-                    onClick = onButtonClick,
-                    toggled = toggledModifiers and KeyMapping.Modifiers.R_CTRL != 0
-                )
-                Button(modifier = btnModifier, data = ExtendedButtons.SpecialKeys.PRINT_SCR, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.SpecialKeys.SCRLK, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.SpecialKeys.PAUSE, onClick = onButtonClick)
-                Button(modifier = btnModifier, data = ExtendedButtons.SpecialKeys.PAGE_DOWN, onClick = onButtonClick)
-            }
-        }
+    val modifier = Modifier
+        .weight(1f)
+        .aspectRatio(1f)
+
+    Row {
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F1, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F2, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F3, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F4, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F5, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F6, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F7, onClick = onButtonClick)
+    }
+    Row {
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F8, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F9, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F10, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F11, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.FRow.F12, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.INSERT, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.PAGE_UP, onClick = onButtonClick)
+    }
+    Row {
+        Button(
+            modifier = modifier,
+            data = ExtendedButtons.Modifiers.L_META,
+            onClick = onButtonClick,
+            toggled = toggledModifiers and KeyMapping.Modifiers.L_META != 0
+        )
+        Button(modifier = modifier,
+            data = ExtendedButtons.Modifiers.R_META,
+            onClick = onButtonClick,
+            toggled = toggledModifiers and KeyMapping.Modifiers.R_META != 0
+        )
+        Button(modifier = modifier,
+            data = ExtendedButtons.Modifiers.R_CTRL,
+            onClick = onButtonClick,
+            toggled = toggledModifiers and KeyMapping.Modifiers.R_CTRL != 0
+        )
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.PRINT_SCR, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.SCRLK, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.PAUSE, onClick = onButtonClick)
+        Button(modifier = modifier, data = ExtendedButtons.SpecialKeys.PAGE_DOWN, onClick = onButtonClick)
     }
 }
 
@@ -184,22 +183,27 @@ fun ExtendedButtonGrid(
                 Icon(imageVector = Icons.Default.KeyboardArrowUp, "")
             }
         }
-        val buttonModifier = Modifier
-            .weight(1f)
-            .aspectRatio(1f)
 
-        AnimatedVisibility(visible = expanded) {
-            ExpandedRow(
-                btnModifier = buttonModifier,
-                toggledModifiers = toggledModifiers,
-                onButtonClick = onButtonClick
-            )
+        Surface(
+            modifier = Modifier.clip(RoundedCornerShape(topStart = 10.dp, topEnd = 10.dp))
+        ) {
+            Column(
+                modifier = Modifier.padding(2.dp)
+            ) {
+                AnimatedVisibility(visible = expanded) {
+                    Column {
+                        ExpandedRows(
+                            toggledModifiers = toggledModifiers,
+                            onButtonClick = onButtonClick
+                        )
+                    }
+                }
+                DefaultRows(
+                    toggledModifiers = toggledModifiers,
+                    onButtonClick = onButtonClick
+                )
+            }
         }
-        DefaultRow(
-            buttonModifier = buttonModifier,
-            toggledModifiers = toggledModifiers,
-            onButtonClick = onButtonClick
-        )
     }
 }
 
--- app/src/main/java/com/onandor/peripheryapp/kbm/ui/components/MultimediaControl.kt
+++ app/src/main/java/com/onandor/peripheryapp/kbm/ui/components/MultimediaControl.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Icon
@@ -44,7 +45,7 @@ private fun MultimediaButton(
 }
 @Composable
 fun MultimediaControl(onButtonClick: (Int) -> Unit) {
-    Surface {
+    Surface(modifier = Modifier.clip(RoundedCornerShape(bottomStart = 10.dp, bottomEnd = 10.dp))) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
