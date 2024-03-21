efa41ac06c7710d646c12d7d40476c437bb6903e
Oláh Nándor <olahnandi00@gmail.com>
2024-03-21 18:35:58 +0100

    Add camera preview

--- app/src/main/java/com/onandor/peripheryapp/webcam/ui/screens/CameraScreen.kt
+++ app/src/main/java/com/onandor/peripheryapp/webcam/ui/screens/CameraScreen.kt
@@ -1,14 +1,50 @@
 package com.onandor.peripheryapp.webcam.ui.screens
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.onandor.peripheryapp.R
+import com.onandor.peripheryapp.webcam.ui.views.CameraPreviewView
 import com.onandor.peripheryapp.webcam.viewmodels.CameraViewModel
 
 @Composable
 fun CameraScreen(
     viewModel: CameraViewModel = hiltViewModel()
 ) {
+    val context = LocalContext.current
+
+    Scaffold { innerPadding ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+        ) {
+            CameraPreviewView(
+                modifier = Modifier.fillMaxSize(),
+                controller = viewModel.getController(context)
+            )
+            IconButton(
+                modifier = Modifier.align(Alignment.BottomEnd),
+                onClick = viewModel::onToggleCamera
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_cameraswitch),
+                    contentDescription = "",
+                    tint = MaterialTheme.colorScheme.surface
+                )
+            }
+        }
+    }
     BackHandler {
         viewModel.navigateBack()
     }
--- app/src/main/java/com/onandor/peripheryapp/webcam/viewmodels/CameraViewModel.kt
+++ app/src/main/java/com/onandor/peripheryapp/webcam/viewmodels/CameraViewModel.kt
@@ -1,14 +1,76 @@
 package com.onandor.peripheryapp.webcam.viewmodels
 
+import android.content.Context
+import android.util.Size
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.resolutionselector.ResolutionSelector
+import androidx.camera.core.resolutionselector.ResolutionStrategy
+import androidx.camera.view.CameraController
+import androidx.camera.view.LifecycleCameraController
 import androidx.lifecycle.ViewModel
 import com.onandor.peripheryapp.navigation.INavigationManager
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.asExecutor
 import javax.inject.Inject
 
 @HiltViewModel
 class CameraViewModel @Inject constructor(
     private val navManager: INavigationManager
 ) : ViewModel() {
+
+    private var controller: LifecycleCameraController? = null
+
+    private val imageAnalyzer = object : ImageAnalysis.Analyzer {
+
+        private var frameSkipCounter = 0
+
+        override fun analyze(image: ImageProxy) {
+            if (frameSkipCounter % 60 != 0) {
+                frameSkipCounter++
+                image.close()
+                return
+            }
+            //println("height: ${image.height}, width: ${image.width}")
+            //println("rotationDegrees: ${image.imageInfo.rotationDegrees}")
+            //println(image.format)
+            frameSkipCounter = 0
+            image.close()
+        }
+    }
+
+    private val resolutionSelector = ResolutionSelector.Builder()
+        .setResolutionStrategy(
+            ResolutionStrategy(
+                Size(1280, 720),
+                ResolutionStrategy.FALLBACK_RULE_CLOSEST_HIGHER_THEN_LOWER
+            )
+        )
+        .build()
+
+    fun getController(context: Context): LifecycleCameraController {
+        if (controller != null) {
+            return controller!!
+        }
+        controller = LifecycleCameraController(context).apply {
+            setEnabledUseCases(CameraController.IMAGE_ANALYSIS)
+            setImageAnalysisAnalyzer(Dispatchers.Main.asExecutor(), imageAnalyzer)
+            imageCaptureResolutionSelector = resolutionSelector
+            imageAnalysisResolutionSelector = resolutionSelector
+        }
+        return controller!!
+    }
+
+    fun onToggleCamera() {
+        controller?.cameraSelector =
+            if (controller?.cameraSelector == CameraSelector.DEFAULT_BACK_CAMERA) {
+                CameraSelector.DEFAULT_FRONT_CAMERA
+            } else {
+                CameraSelector.DEFAULT_BACK_CAMERA
+            }
+    }
 
     fun navigateBack() {
         navManager.navigateBack()
