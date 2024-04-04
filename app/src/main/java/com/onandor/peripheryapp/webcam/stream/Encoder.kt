740deb871e043c8ada63e7e86ba61958d1f81c14
Oláh Nándor <olahnandi00@gmail.com>
2024-04-04 23:04:56 +0200

    Send SPS and PPS NALU before every key frame

--- app/src/main/java/com/onandor/peripheryapp/webcam/stream/Encoder.kt
+++ app/src/main/java/com/onandor/peripheryapp/webcam/stream/Encoder.kt
@@ -2,24 +2,14 @@ package com.onandor.peripheryapp.webcam.stream
 
 import android.media.MediaCodec
 import android.media.MediaFormat
-import android.media.MediaMuxer
-import android.os.Environment
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.io.ByteArrayOutputStream
-import java.io.File
-import java.io.FileOutputStream
-import java.nio.ByteBuffer
 
 class Encoder(
     private val mediaCodec: MediaCodec,
     private val onDataEncoded: (ByteArray) -> Unit,
 ) {
 
-    private var spsPpsInfo: ByteArray? = null
-    private val outputStream = ByteArrayOutputStream()
-    private var spsPps: ByteArray? = null
+    private var spsPpsNalu: ByteArray? = null
 
     private val mediaCodecCallback = object : MediaCodec.Callback() {
         override fun onInputBufferAvailable(codec: MediaCodec, index: Int) {}
@@ -41,10 +31,11 @@ class Encoder(
         override fun onOutputFormatChanged(codec: MediaCodec, format: MediaFormat) {
             val sps = format.getByteBuffer("csd-0")
             val pps = format.getByteBuffer("csd-1")
-            outputStream.write(sps?.array())
-            outputStream.write(pps?.array())
-            onDataEncoded(outputStream.toByteArray())
-            outputStream.reset()
+            if (sps == null || pps == null) {
+                return
+            }
+
+            spsPpsNalu = sps.array() + pps.array()
         }
     }
 
@@ -62,40 +53,20 @@ class Encoder(
             outputBuffer[outData]
         }
 
-        if ((bufferInfo.flags and MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0) {
-            mediaCodec.releaseOutputBuffer(index, false)
+        mediaCodec.releaseOutputBuffer(index, false)
+
+        if ((bufferInfo.flags and MediaCodec.BUFFER_FLAG_CODEC_CONFIG) != 0 ||
+            spsPpsNalu == null) {
+            // Configuration change is handled in the callback
             return null
         }
 
-        if (spsPpsInfo == null) {
-            val spsPpsBuffer = ByteBuffer.wrap(outData)
-            if (spsPpsBuffer.getInt() == 0x00000001) {
-                spsPpsInfo = ByteArray(outData.size)
-                spsPpsInfo = outData.copyOf()
-            } else {
-                return null
-            }
+        // Insert SPS and PPS NALU before every key frame to be sure
+        return if ((bufferInfo.flags and MediaCodec.BUFFER_FLAG_KEY_FRAME) != 0) {
+            spsPpsNalu!! + outData
         } else {
-            outputStream.write(outData)
+            outData
         }
-
-        mediaCodec.releaseOutputBuffer(index, false)
-
-        var ret = outputStream.toByteArray()
-        /*
-        if (ret.size > 5 && ret[4] == 0x65.toByte()) {
-            println("insert SPS PPS info")
-            outputStream.reset()
-            outputStream.write(spsPpsInfo)
-            outputStream.write(ret)
-            ret = outputStream.toByteArray()
-        }
-         */
-        //println("bufferInfo.size: " + bufferInfo.size + ", bufferInfo.offset: " + bufferInfo.offset)
-        //muxer.writeSampleData(trackIndex, ByteBuffer.wrap(outData), bufferInfo)
-        //fos.write(outData)
-        outputStream.reset()
-        return outData
     }
 
     fun release() {
