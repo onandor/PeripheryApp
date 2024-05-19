3af1476090de7f0f28ab14052a869ad51f802d78
Oláh Nándor <olahnandi00@gmail.com>
2024-05-19 12:58:40 +0200

    Compress jpeg frames

--- app/src/main/java/com/onandor/peripheryapp/webcam/video/encoders/JpegEncoder.kt
+++ app/src/main/java/com/onandor/peripheryapp/webcam/video/encoders/JpegEncoder.kt
@@ -1,11 +1,15 @@
 package com.onandor.peripheryapp.webcam.video.encoders
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.graphics.ImageFormat
 import android.media.Image
 import android.media.ImageReader
 import android.os.Handler
 import android.os.HandlerThread
 import android.view.Surface
+import java.io.ByteArrayInputStream
+import java.io.ByteArrayOutputStream
 
 class JpegEncoder(
     val width: Int,
@@ -23,10 +27,31 @@ class JpegEncoder(
 
     private val mImageListener = ImageReader.OnImageAvailableListener { reader ->
         val image: Image = reader.acquireNextImage()
-        val outFrame = ByteArray(image.planes[0].buffer.remaining())
-        image.planes[0].buffer[outFrame]
-        onDataReady(outFrame)
+        val frame = ByteArray(image.planes[0].buffer.remaining())
+        image.planes[0].buffer[frame]
+        val compressedFrame = compress(frame)
+        compressedFrame?.let(onDataReady)
         image.close()
+    }
+
+    private fun compress(frame: ByteArray): ByteArray? {
+        val inputStream = ByteArrayInputStream(frame)
+        val bitmapOptions = BitmapFactory.Options().apply {
+            inJustDecodeBounds = false
+        }
+        val bitmap = BitmapFactory.decodeStream(inputStream, null, bitmapOptions)
+        inputStream.close()
+        if (bitmap == null) {
+            return null
+        }
+
+        val outputStream = ByteArrayOutputStream()
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 50, outputStream)
+        bitmap.recycle()
+
+        val jpeg = outputStream.toByteArray()
+        outputStream.close()
+        return jpeg
     }
 
     override fun start() {
